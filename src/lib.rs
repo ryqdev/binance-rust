6857d0cfcaad6075b373c19d088f0c2268f5040b
ryqdev <ykshang4119@gmail.com>
2024-03-30 23:56:03 +0800

    make it public

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod util;
 
-fn binance_price() -> anyhow::Result<String> {
+pub fn binance_price() -> anyhow::Result<String> {
     let body = util::fetch_price()?;
     let price = util::fetch_price_from_json(&body)?;
     Ok(price)
